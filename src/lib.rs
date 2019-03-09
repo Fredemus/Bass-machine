25ff866088e16313535bb29337c1b8491902c6df
Fredemus <46689307+Fredemus@users.noreply.github.com>
2019-03-09 02:07:41 +0100

    Update lib.rs
    Added functions for receiving midi and implemented note on/off functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,51 @@
 /*
+
     TODO:
         Make sure we stay inside of bounds (limit self.pos with duration function. (reader.duration))
         Implement note on/off functionality
+        
         
 */
 
 //vst stuff
  #[macro_use] extern crate vst;
+use vst::api::{Events, Supported};
 use vst::buffer::AudioBuffer;
-
-use vst::plugin::{Info, Plugin, Category};  
-
-//filereading stuff
-
-
-//reading into array stuff
-// Add extern crate declaration (at the root of your crate)
-
-
+use vst::event::Event;
+use vst::plugin::{CanDo, Info, Plugin, Category};  
 
 struct WaveTable
 {
     wave_buffer : Vec<f32>,
     //file : File,
     it : usize,
-    pos : usize
+    pos : usize,
+    //for midi handling
+    note: Option<u8>,
+    note_duration: f64,
+    
+}
+
+impl WaveTable
+{
+    fn process_midi_event(&mut self, data: [u8; 3]) {
+        match data[0] {
+            128 => self.note_off(data[1]),
+            144 => self.note_on(data[1]),
+            _ => (),
+        }
+    }
+
+    fn note_on(&mut self, note: u8) {
+        self.note_duration = 0.0;
+        self.note = Some(note)
+    }
+
+    fn note_off(&mut self, note: u8) {
+        if self.note == Some(note) {
+            self.note = None
+        }
+    }
 }
 
 impl Default for WaveTable
@@ -37,6 +58,8 @@ impl Default for WaveTable
             wave_buffer : vec![0.],
             it : 0,
             pos : 1,
+            note_duration: 0.0,
+            note: None,
         };
         let mut reader = hound::WavReader::open(r"C:\Users\rasmu\Documents\Xfer\Serum Presets\Tables\Analog\Basic Shapes.wav").unwrap();
         a.wave_buffer = reader.samples().collect::<Result<Vec<f32>,_>>().unwrap();
@@ -44,7 +67,6 @@ impl Default for WaveTable
         return a;
     }
 }
-
 
 impl Plugin for WaveTable
 {
@@ -58,6 +80,19 @@ impl Plugin for WaveTable
             category: Category::Synth,
             parameters: 1,
             ..Default::default()
+        }
+    }
+    // Supresses warning about match statment only having one arm
+    #[allow(unknown_lints)]
+    #[allow(unused_variables)]
+    #[allow(clippy::single_match)]
+    fn process_events(&mut self, events: &Events) {
+        for event in events.events() {
+            match event {
+                Event::Midi(ev) => self.process_midi_event(ev.data),
+                // More events can be handled here.
+                _ => (),
+            }
         }
     }
     fn get_parameter(&self, index: i32) -> f32 {
@@ -106,16 +141,29 @@ impl Plugin for WaveTable
         // let stereo_out = l[0].iter_mut().zip(r[0].iter_mut());
         for output_channel in outputs.into_iter()  {
                 for output_sample in output_channel {
-                    if self.it >= 2048
-                    {
-                        self.it = 0
+                    if let Some(current_note) = self.note {
+                        if self.it >= 2048
+                        {
+                            self.it = 0
+                        }
+                        *output_sample = self.wave_buffer[self.it + (2048 * self.pos)] * 0.5;
+                        //*output_sample = 1.;
+                        self.it += 1;
                     }
-                    *output_sample = self.wave_buffer[self.it + (2048 * self.pos)] * 0.5;
-                    //*output_sample = 1.;
-                    self.it += 1;
+                    else {
+                        //behavior of it at note off can be seen as starting phase, and could be made a variable
+                        self.it = 0;
+                        *output_sample = 0.;
+                    }
 
                 }
             }
+    }
+    fn can_do(&self, can_do: CanDo) -> Supported {
+        match can_do {
+            CanDo::ReceiveMidiEvent => Supported::Yes,
+            _ => Supported::Maybe,
+        }
     }
 }
 plugin_main!(WaveTable);
