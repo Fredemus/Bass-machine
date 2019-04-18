63d319a799152560689574573483b301afe05975
Fredemus <46689307+Fredemus@users.noreply.github.com>
2019-04-19 00:23:11 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
 /*
 
     TODO:
-        Make sure we stay inside of bounds (limit self.pos with duration function. (reader.duration))
-        Implement note on/off functionality
-        
-        
+    violent jumps (e.g. from 1 to -1 in a square) causes the interpolation to make enormous overshoots at the value. Saw a sample at 804 lol
+    
+    
+
+
 */
 
 //vst stuff
@@ -14,33 +15,56 @@ use vst::buffer::AudioBuffer;
 use vst::event::Event;
 use vst::plugin::{CanDo, Info, Plugin, Category};  
 
+extern crate hound;
+
+// impl FirFilter
+// {
+//     //needs a calc_coefficients to put cutoff at the right place.
+// }
+
+//include interpolation module:
+mod interp;
+
 struct WaveTable
 {
-    wave_buffer : Vec<f32>,
+    //wave_buffer : Vec<f32>,
+    //pitched_buffer: Vec<f32>,
+    //time_vec : Vec<f32>,
     //file : File,
     it : usize,
     pos : usize,
     //for midi handling
     note: Option<u8>,
     note_duration: f64,
-    
+    sample_rate: f32,
+    interpolator : interp::Interp, 
+    //interpolator : interp::Interp,
+    wt_len : usize,
 }
 
 impl WaveTable
 {
+    fn find_ratio(&self, note : u8) -> f32 {
+        let standard = 21.827; //default wavetable pitch
+        let pn = 440f32 * (2f32.powf(1./12.)).powi(note as i32 - 69);
+        //return ratio between desired pitch and standard 
+        standard / pn
+    }
+
     fn process_midi_event(&mut self, data: [u8; 3]) {
         match data[0] {
             128 => self.note_off(data[1]),
             144 => self.note_on(data[1]),
             _ => (),
         }
+        //change pitched_buffer here?
     }
-
     fn note_on(&mut self, note: u8) {
         self.note_duration = 0.0;
-        self.note = Some(note)
+        self.note = Some(note);
+        //self.interpolator.cubic_interpolation(self.find_ratio(note))
+        self.interpolator.cubic_interpolation(self.find_ratio(note))
     }
-
     fn note_off(&mut self, note: u8) {
         if self.note == Some(note) {
             self.note = None
@@ -50,19 +74,25 @@ impl WaveTable
 
 impl Default for WaveTable
 {
-       
-
     fn default() -> WaveTable {
 
         let mut a = WaveTable {
-            wave_buffer : vec![0.],
+            //wave_buffer : vec![0.],
+            //pitched_buffer: vec![0.],
             it : 0,
             pos : 1,
             note_duration: 0.0,
             note: None,
+            sample_rate: 44100.,
+            interpolator : Default::default(),
+            wt_len : 7,
         };
         let mut reader = hound::WavReader::open(r"C:\Users\rasmu\Documents\Xfer\Serum Presets\Tables\Analog\Basic Shapes.wav").unwrap();
-        a.wave_buffer = reader.samples().collect::<Result<Vec<f32>,_>>().unwrap();
+        a.interpolator.source_y = reader.samples().collect::<Result<Vec<_>,_>>().unwrap();
+        a.interpolator.calc_coefficients();
+        
+        //a.pitched_buffer = a.wave_buffer.iter().step_by(2).clone();/*collect::<Vec<f32>>();*/
+        //a.pitched_buffer = reader.samples().step_by(2).collect::<Result<Vec<_>,_>>().unwrap();
 
         return a;
     }
@@ -70,6 +100,9 @@ impl Default for WaveTable
 
 impl Plugin for WaveTable
 {
+    fn set_sample_rate(&mut self, rate: f32) {
+        self.sample_rate = rate;
+    }
     fn get_info(&self) -> Info 
     {
         Info  {
@@ -82,10 +115,6 @@ impl Plugin for WaveTable
             ..Default::default()
         }
     }
-    // Supresses warning about match statment only having one arm
-    #[allow(unknown_lints)]
-    #[allow(unused_variables)]
-    #[allow(clippy::single_match)]
     fn process_events(&mut self, events: &Events) {
         for event in events.events() {
             match event {
@@ -101,15 +130,12 @@ impl Plugin for WaveTable
         _ => 0.0,
         }
     }
-
     fn set_parameter(&mut self, index: i32, value: f32) {
         match index {
-            0 => self.pos = ((value * 4.).round()) as usize,
+            0 => self.pos = ((value * (self.wt_len - 1) as f32).round()) as usize,
             _ => (),
         }
-
     }
-
     fn get_parameter_name(&self, index: i32) -> String {
         match index {
             0 => "WT pos".to_string(),
@@ -123,7 +149,6 @@ impl Plugin for WaveTable
             _ => "".to_string(),
         }
     }
-
     fn process(&mut self, buffer: &mut AudioBuffer<f32>) {
 
         // Split out the input and output buffers into two vectors
@@ -139,25 +164,37 @@ impl Plugin for WaveTable
         //  // Iterate over outputs as (&mut f32, &mut f32)
         // let (mut l, mut r) = outputs.split_at_mut(1);
         // let stereo_out = l[0].iter_mut().zip(r[0].iter_mut());
-        for output_channel in outputs.into_iter()  {
-                for output_sample in output_channel {
-                    if let Some(current_note) = self.note {
-                        if self.it >= 2048
-                        {
-                            self.it = 0
-                        }
-                        *output_sample = self.wave_buffer[self.it + (2048 * self.pos)] * 0.5;
-                        //*output_sample = 1.;
-                        self.it += 1;
+    for output_channel in outputs.into_iter()  {
+            for output_sample in output_channel {
+                if let Some(current_note) = self.note {
+                    
+                    if self.it >= (self.interpolator.new_len/self.wt_len - 1)
+                    {
+                        self.it = 0
                     }
-                    else {
-                        //behavior of it at note off can be seen as starting phase, and could be made a variable
-                        self.it = 0;
-                        *output_sample = 0.;
-                    }
+                    self.find_ratio(self.note.unwrap());
+                    *output_sample = self.interpolator.interpolated[self.it + ((self.interpolator.new_len/self.wt_len) * self.pos)] ;
+                    //*output_sample = 1.;
+                    self.it += 1;
+                    
+                    // if self.it >= (2048 - 1)
+                    // {
+                    //     self.it = 0
+                    // }
+                    // *output_sample = self.interpolator.interpolated[self.it + ((2048) * self.pos)] ;
+                    // //*output_sample = 1.;
+                    // self.it += 1;
+                    
+                }
+                else {
+                    //behavior of it at note off can be seen as starting phase, and could be made a variable
+                    self.it = 0;
+                    *output_sample = 0.;
 
                 }
+               
             }
+        }
     }
     fn can_do(&self, can_do: CanDo) -> Supported {
         match can_do {
