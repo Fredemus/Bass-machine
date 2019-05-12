6fc31c882412f99f49d93ece5e54f31a58e4ddcd
Fredemus <46689307+Fredemus@users.noreply.github.com>
2019-05-12 03:09:52 +0200

    Added proper slicing, made step_one function

--- src/interp.rs
+++ src/interp.rs
@@ -2,48 +2,65 @@
 #[allow(dead_code)]
 pub struct Interp
 {
-
     pub(crate) source_y : Vec<f32>,
     pub(crate) waveforms : Vec<Vec<f32>>,
     wave_number : usize,
-    wave_len : usize,
+    pub wave_len : usize,
     pub(crate) len : usize,
-    pub(crate) times_oversampled : usize,
+    pub(crate) amt_oversample : usize,
     pub(crate) new_len : usize,
     //coeffs : Vec<Vec<f32>>, //hopefully this can make 2 vectors of f32
-    c0: Vec<f32>,
-    c1: Vec<f32>,
-    c2: Vec<f32>,
-    c3: Vec<f32>,
+    c0: Vec<Vec<f32>>, c1: Vec<Vec<f32>>, c2: Vec<Vec<f32>>, c3: Vec<Vec<f32>>,
     pub(crate) interpolated: Vec<f32>,
-    upsample_fir : Vec<f32>,
+    pub it : usize,
+    pub pos : usize,
+    pub(crate) upsample_fir : Vec<f32>,
 }
 #[allow(dead_code)]
 impl Interp
 {
-    pub(crate) fn oversample(&mut self, ratio: usize){
-        self.times_oversampled = ratio;
-        self.len = self.source_y.len();
-        let mut temp = vec![0.];
-        temp.resize(self.len * ratio, 0.);
-        for i in 0..(self.len * ratio) {
-            if i % 2 == 0 {
-                temp[i] = self.source_y[i/2];
-            }
-            else {
-                temp[i] = 0.;
-            }
+    //returns the next sample to be played
+    pub fn step_one(& mut self) -> f32 {
+        if self.it >= ((self.new_len - 1 ) / self.amt_oversample )
+        {
+            self.it = 0
         }
-        //self.len = self.source_y.len();
-        //resize source y to fit the new length
-        self.source_y.resize(self.len * ratio, 0.); 
-        self.len = self.source_y.len();
+        //naive downsampling for now, implement a (halfband?) filter here
+        //if self.it % 2 == 0 {
+            let output = self.interpolated[self.it * self.amt_oversample /*+ 
+            (((self.new_len)/self.wave_number)  * self.pos)*/] ;
+            //*output_sample = 1.;
+            self.it += 1;
+        //}
+        output
+    }
+    //oversamples the wavetable by the ratio
+    pub(crate) fn oversample(&mut self, ratio: usize){
+        self.amt_oversample = ratio;
+        //resize slices to fit the new length
+        for i in 0..self.wave_number {
+            self.waveforms[i].resize(self.wave_len * ratio, 0.);
+        }
+        let mut temp = vec![0.];
+        temp.resize(self.wave_len * ratio, 0.);
+        for i in 0..self.wave_number {
+            //fills temp with an oversampled version of current slice
+            for j in 0..(self.wave_len * ratio) {
+                if j % ratio == 0 {
+                    temp[j] = self.waveforms[i][j/ratio];
+                }
+                else {
+                    temp[j] = 0.;
+                }
+            }
+            self.waveforms[i] = temp.to_vec();
+        }
         //convolve zero-stuffed vector with coefficients (sinc) of a fir, to remove mirror images above new_Fs/4 
         //upsample_fir could be turned into a polyphase implementation, to halve number of clock cycles needed
-        self.source_y = self.convolve(&self.upsample_fir, temp);
-        
+        for i in 0..self.wave_number {
+            self.waveforms[i] = self.convolve(&self.upsample_fir, &self.waveforms[i]);
+        }
     }
-
     //slices the read .wav into individual waveforms
     pub(crate) fn slice(&mut self) {
         self.len = self.source_y.len();
@@ -57,10 +74,10 @@ impl Interp
             }
         }
     }
-    //check for off-by-ones at some point. self.len should be fine instead of len_x
+    //finds the coefficients necessary for Hermite interpolation
     pub(crate) fn calc_coefficients(&mut self) {
         self.len = self.source_y.len();
-        let len_x = self.len - 1;
+        let new_wave_len = self.wave_len *self.amt_oversample;
         /*
         // 4-point, 3rd-order Hermite (x-form)
         float c0 = y[0];
@@ -68,59 +85,81 @@ impl Interp
         float c2 = y[-1] - 5/2.0*y[0] + 2*y[1] - 1/2.0*y[2];
         float c3 = 1/2.0*(y[2]-y[-1]) + 3/2.0*(y[0]-y[1]);
         return ((c3*x+c2)*x+c1)*x+c0;
-        */
-        self.c0.resize(self.len, 0.);
-        self.c1.resize(self.len, 0.);
-        self.c2.resize(self.len, 0.);
-        self.c3.resize(self.len, 0.);
-
-
+        */ 
+        self.c0.resize(self.wave_number,vec![0.;new_wave_len]);
+        self.c1.resize(self.wave_number,vec![0.;new_wave_len]);
+        self.c2.resize(self.wave_number,vec![0.;new_wave_len]);
+        self.c3.resize(self.wave_number,vec![0.;new_wave_len]);
         //this could easily be optimized away, but oh well
-        for i in 0..len_x {
-                self.c0[i] = self.source_y[i];
+        for i in 0..self.wave_number {
+            for j in 0..new_wave_len {
+                self.c0[i][j] = self.waveforms[i][j];
             }
-        //instead of len_x it should be 0+cyclelength. doesn't seem to be a big problem
-        self.c1[0] =  1./2.0*(self.source_y[0+1] - self.source_y[len_x]);
-        self.c2[0] =  self.source_y[len_x] - 5./2.0*self.source_y[0] + 2.*self.source_y[0+1] - 1.0/2.0*self.source_y[0+2];
-        self.c3[0] =  1./2.0*(self.source_y[0+2]-self.source_y[len_x]) + 3.0/2.0*(self.source_y[0+0]-self.source_y[0+1]);
-        for i in 1..len_x {
-                self.c1[i] =  1./2.0*(self.source_y[i+1] -self.source_y[i-1]);
+        }
+        for i in 0..self.wave_number {
+            for j in 1..new_wave_len - 1 {
+                
+                self.c1[i][j] =  1./2.0*(self.waveforms[i][j+1] -self.waveforms[i][j-1]);
             }
-        for i in 1..len_x-1 {
-                self.c2[i] =  self.source_y[i-1] - 5./2.0*self.source_y[i] + 2.*self.source_y[i+1] - 1.0/2.0*self.source_y[i+2];
+        }
+        for i in 0..self.wave_number {
+            for j in 1..new_wave_len - 2 {
+                self.c2[i][j] =  self.waveforms[i][j-1] - 5./2.0*self.waveforms[i][j] + 2.*self.waveforms[i][j+1] - 1.0/2.0*self.waveforms[i][j+2];
             }
-        for i in 1..len_x-1 {
-                self.c3[i] =  1./2.0*(self.source_y[i+2]-self.source_y[i-1]) + 3.0/2.0*(self.source_y[i+0]-self.source_y[i+1]);
+        }
+        for i in 0..self.wave_number {
+            for j in 1..new_wave_len - 2 {
+                self.c3[i][j] =  1./2.0*(self.waveforms[i][j+2]-self.waveforms[i][j-1]) + 3.0/2.0*(self.waveforms[i][j+0]-self.waveforms[i][j+1]);
             }
-        //instead of 0 it should be len_x-cyclelength.
-        self.c2[len_x] =  self.source_y[len_x-1] - 5./2.0*self.source_y[len_x] + 2.*self.source_y[0] - 1.0/2.0*self.source_y[1];
-        self.c3[len_x] =  1./2.0*(self.source_y[1]-self.source_y[len_x-1]) + 3.0/2.0*(self.source_y[len_x+0]-self.source_y[0]);
+        }
+        //makes sure the start of waveforms are handled properly
+        for i in 0..self.wave_number {
+            self.c1[i][0] =  (1.0/2.0)*(self.waveforms[i][0+1] - self.waveforms[i][new_wave_len  - 1]);
+            self.c2[i][0] =  self.waveforms[i][new_wave_len  - 1] - (5./2.0)*self.waveforms[i][0] + 2.*self.waveforms[i][0+1] - (1.0/2.0)*self.waveforms[i][0+2];
+            self.c3[i][0] =  (1.0/2.0)*(self.waveforms[i][0+2]-self.waveforms[i][new_wave_len  - 1]) + (3.0/2.0)*(self.waveforms[i][0]-self.waveforms[i][0+1]);
+        }
+        //makes sure the end of waveforms are handled properly
+        for i in 0..self.wave_number {
+            self.c1[i][new_wave_len  - 1] =  1./2.0*(self.waveforms[i][0] - self.waveforms[i][new_wave_len - 2]);
+            self.c2[i][new_wave_len - 1] =  self.waveforms[i][new_wave_len - 2] - 5./2.0*self.waveforms[i][new_wave_len - 1] + 2.*self.waveforms[i][0] - 1.0/2.0*self.waveforms[i][0+1];
+            self.c2[i][new_wave_len - 2] =  self.waveforms[i][new_wave_len - 3] - 5./2.0*self.waveforms[i][new_wave_len - 2] + 2.*self.waveforms[i][new_wave_len - 1] - 1.0/2.0*self.waveforms[i][0];
+            self.c3[i][new_wave_len - 1] =  1./2.0*(self.waveforms[i][0+1]-self.waveforms[i][new_wave_len - 2]) + 3.0/2.0*(self.waveforms[i][new_wave_len - 1]-self.waveforms[i][0]);
+            self.c3[i][new_wave_len - 2] =  1./2.0*(self.waveforms[i][0]-self.waveforms[i][new_wave_len - 3]) + 3.0/2.0*(self.waveforms[i][new_wave_len - 2]-self.waveforms[i][new_wave_len - 1]);
+        }
+        // for i in 1..len_x-1 {
+        //         self.c3[i] =  1./2.0*(self.source_y[i+2]-self.source_y[i-1]) + 3.0/2.0*(self.source_y[i+0]-self.source_y[i+1]);
+        //     }
+      
     }
-
+    //consider fixing some discontinuities by just setting first and last sample to original lol
+    //only interpolates one waveform at a time. This means for now that you cant change waveform without changing notes.
     pub(crate) fn interpolation(&mut self,ratio: f32 ) {
         let mut temp : f32;
         let mut it : usize;
         //find et x-array ud fra hvor mange samples der skal til for at nå den ratio
-        self.new_len = ((self.len as f32) * ratio).round() as usize;
+        //self.new_len = ((self.len as f32) * ratio).round() as usize;
+        self.new_len = ((self.wave_len as f32) * self.amt_oversample as f32 *ratio).round() as usize;
         //resize the vector to the new size
         self.interpolated.resize(self.new_len, 0.);
-
         //this should describe all the necessary values of x, since x always should be between 0 and 1
         let x = 1. / ratio;
         for i in 0..(self.new_len -1) {
             it = ((i as f32) * x) as usize;
-            temp = ((self.c3[it]*x+self.c2[it])*x+self.c1[it])*x+self.c0[it];
+            temp = ((self.c3[self.pos][it]*x+self.c2[self.pos][it])*x+self.c1[self.pos][it])*x+self.c0[self.pos][it];
+
+            //clipping ameliorates a problem with overshoots from this interpolation algorithm.
             if temp < 0. {
-                self.interpolated[i] = temp.max(-1.);
+                self.interpolated[i] = temp.max(-1.1);
             }
             else {
-                self.interpolated[i] = temp.max(-1.);
+                self.interpolated[i] = temp.min(1.1);
             }
         }
+        self.interpolated[0] = self.waveforms[self.pos][0];
     }
-    pub(crate) fn convolve(&self, p_coeffs : &Vec<f32>, p_in : Vec<f32>) -> Vec<f32>
-    {
-       
+    pub(crate) fn convolve(&self, p_coeffs : &Vec<f32>, p_in : &Vec<f32>) -> Vec<f32>
+    {   //possibly more efficient convolution https://stackoverflow.com/questions/8424170/1d-linear-convolution-in-ansi-c-code
+        //convolution could be significantly sped up by doing it in the frequency domain. from O(n^2) to O(n*log(n))
         let mut convolved : Vec<f32>;
         let new_len = p_in.len() + (p_coeffs.len() - 1)/2 ;
         convolved = vec![0.;p_in.len() + p_coeffs.len()];
@@ -130,24 +169,20 @@ impl Interp
         //n should be the length of p_in + length of p_coeffs
         for k in 0..(new_len)  //  position in output
         {
-            //convolved[k] = 0.;
-
             for i in 0..p_coeffs.len()  //  position in coefficients array
-            {   //this might even take care of group-delay
-                if k >= i //&& k < p_in.len()
+            {   
+                if k >= i 
                 {
                     convolved[k] += p_coeffs[i] * temp[k - i];
-
                 }
-
             }
         }
-        //trimming the sample
-        //remove initial group delay by taking number of coefficients - 1 / 2. Only works for number of coefficients
+        //trimming the result
+        //remove initial group delay by taking number of coefficients - 1 / 2. Only works for odd number of coefficients
         for _i in 0..(p_coeffs.len() - 1)/2 {
             convolved.remove(0); //maybe use drain on an iterator instead?
         }
-        //trims unnecessary sample at the end
+        //trims unnecessary samples at the end
         convolved.truncate(p_in.len());
         return convolved;
     }
@@ -161,8 +196,10 @@ impl Default for Interp
             len : 0,
             new_len: 0,
             wave_number : 0,
-            times_oversampled : 0,
+            amt_oversample : 1,
             wave_len : 2048,
+            it : 0,
+            pos: 2,
             //coeffs : Vec<Vec<f32>>, //hopefully this can make 2 vectors of f32
             //default capacity should take oversampling into account
             c0: Vec::with_capacity(2048 * 256 * 2),
